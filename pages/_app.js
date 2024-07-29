4a6139426f2ca4d4af6f4db19920d1a819e9c9f0
Mark Brooks <markabrooks@gmail.com>
2024-07-29 13:56:30 -0600

    add vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/global.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export default function App({ Component, pageProps }) {
     return ( 
       <>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </>
     )
   }
