bc950f5b7927029489797f99911ac60c8354b2ab
Mark Brooks <markabrooks@gmail.com>
2024-08-14 11:52:21 -0600

    add blog post (#5)

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import '../styles/global.css';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import '../styles/global.css'
+import 'highlight.js/styles/tokyo-night-dark.css'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function App({ Component, pageProps }) {
-    return ( 
-      <>
-        <Component {...pageProps} />
-        <Analytics />
-        <SpeedInsights />
-      </>
-    )
-  }
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
+}
