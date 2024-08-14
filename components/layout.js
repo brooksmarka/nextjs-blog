6b7cfb0bf386132dd36aea501871d60dec810469
Mark Brooks <markabrooks@gmail.com>
2024-08-14 13:04:03 -0600

    add copyright to bottom

--- components/layout.js
+++ components/layout.js
@@ -1,69 +1,73 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/layout.module.css'
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import Navbar from './navbar';
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Navbar from './navbar'
 
-const name = 'Mark Brooks';
-const shortBio1 =  'Software Engineer based out of Arvada Colorado.'
-const shortBio2 =  'I enjoy creating applications that bridge the gap between the outdoors and technology.'
-export const siteTitle = 'Mark Brooks - Software Engineer';
+const name = 'Mark Brooks'
+const shortBio1 = 'Software Engineer based out of Arvada Colorado.'
+const shortBio2 =
+  'I enjoy creating applications that bridge the gap between the outdoors and technology.'
+export const siteTitle = 'Mark Brooks - Software Engineer'
 
-export default function Layout({ children, home}) {
+export default function Layout({ children, home }) {
   return (
-      <div className={styles.container}>
+    <div className={styles.container}>
       <Navbar />
-        <Head>
-          <link rel="icon" href="/images/profile.jpg" />
-          <meta
-            name="description"
-            content="The personal blog for Mark Brooks a developer out of Colorado."
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <header className={styles.header}>
-          {home ? (
-            <>
+      <Head>
+        <link rel="icon" href="/images/profile.jpg" />
+        <meta
+          name="description"
+          content="The personal blog for Mark Brooks a developer out of Colorado."
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            <h2 className={utilStyles.headingMd}>{shortBio1}</h2>
+            <h2 className={utilStyles.headingMd}>{shortBio2}</h2>
+          </>
+        ) : (
+          <>
+            <Link href="/">
               <Image
                 priority
                 src="/images/profile.jpg"
                 className={utilStyles.borderCircle}
-                height={144}
-                width={144}
+                height={108}
+                width={108}
                 alt=""
               />
-              <h1 className={utilStyles.heading2Xl}>{name}</h1>
-              <h2 className={utilStyles.headingMd}>{shortBio1}</h2>
-              <h2 className={utilStyles.headingMd}>{shortBio2}</h2>
-            </>
-          ) : (
-            <>
-              <Link href="/">
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt=""
-                />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
               </Link>
-              <h2 className={utilStyles.headingLg}>
-                <Link href="/" className={utilStyles.colorInherit}>
-                  {name}
-                </Link>
-              </h2>
-            </>
-          )}
-        </header>
-        <main>{children}</main>
-        {!home && (
-          <div className={styles.backToHome}>
-            <Link href="/">← Back to home</Link>
-          </div>
+            </h2>
+          </>
         )}
+      </header>
+      <main className={styles.main}>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+      <div className={styles.footer}>
+        &copy; 2024 Mark Brooks. All rights reserved.
       </div>
-  );
+    </div>
+  )
 }
